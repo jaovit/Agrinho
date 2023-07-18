ef3645c841dbbad1885e21d33809cc237d756c59
jão <110256135+jaovit@users.noreply.github.com>
2023-07-17 23:50:01 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,67 +1,51 @@
 var modo = true;
-// true = escuro;
-// false = claro;
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    modo = true;
+    document.querySelector('.template').classList.remove("claro");
+    console.log("modo escuro")
+    // true = escuro;
+
+} else {
+    modo = false
+    document.querySelector('.template').classList.add("claro");
+    console.log("modo claro")
+    // false = claro;
+}
 
 function mudarRoot() {
     var root = document.documentElement;
     if (modo) {
         var mudancas = {
+            // claro
             '--cor-principal': '#c7c7c7',
             '--cor-segundaria': '#b6b6b6',
             '--cor-titulo': '#202020',
             '--sub-titulo': '#242424',
             '--cor-texto': '#2e2e2e',
-            '--cor-borda': 'solid .01vw rgb(70, 70, 70)',
+            '--borda': 'solid .01vw rgb(70, 70, 70)',
             '--background-card': 'url(../img/oatmeal-01_4e7de307.jpg)',
             '--filtro': 'brightness(50%)'
         }
 
-        document.querySelector('.template').style.cssText =
-            'background-color: var(--cor-segundaria);' +
-            'height: 40vw;'
-
-        document.querySelector('.template_img').style.cssText =
-            'display: block;'
-
-        document.querySelector('.texto_1').style.cssText =
-            ' top: 50%; ' +
-            ' left: 18vw; ' +
-            ' translate: 0 -50%; ' +
-            ' width: 35vw; '
+        document.querySelector('.template').classList.add("claro");
 
         modo = false
     } else {
+        // escuro
         var mudancas = {
             '--cor-principal': '#302D2C',
             '--cor-segundaria': '#464342',
             '--cor-titulo': '#ebc4b0',
             '--sub-titulo': '#EBCCBB',
             '--cor-texto': '#c9c2c0',
-            '--cor-borda': 'solid .01vw rgb(172, 170, 170)',
+            '--borda': 'solid .01vw rgb(172, 170, 170)',
             '--background-card': 'url(../img/mocha-01_3eb3a5cd.jpg)',
             '--filtro': 'brightness(35%)'
         }
-        document.querySelector('.template').style.cssText =
-            'background-color: transparent;' +
-            'height: 38vw;'
 
-        document.querySelector('.template_img').style.cssText =
-            'display: none;'
+        document.querySelector('.template').classList.remove("claro");
 
-        document.querySelector('.texto_1').style.cssText =
-            ' top: 11vw; ' +
-            ' left: 50%;' +
-            ' translate: -50% 0;' +
-            ' width: 70vw;'
-
-        // document.querySelector('.topicos').style.cssText =
-        //     'height: 22vw;;'
-
-        // document.querySelector('#topic_t').style.cssText =
-        //     'top: 0;'
-
-        // document.querySelector('.topicos_lista').style.cssText =
-        //     'top: 3vw;'
         modo = true
     }
 
