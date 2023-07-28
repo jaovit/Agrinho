0ac3d2d343efe12fa4bcb6c394dd8645a68b1298
jão <110256135+jaovit@users.noreply.github.com>
2023-07-28 11:59:42 -0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -39,53 +39,53 @@ nav.addEventListener('click', function (event) {
     event.stopPropagation();
 });
 
-const searchInput = document.getElementById('searchInput');
-const allTextElements = document.querySelectorAll('.titulo, .subtitulo, .texto');
+// const searchInput = document.getElementById('searchInput');
+// const allTextElements = document.querySelectorAll('.titulo, .subtitulo, .texto');
 
-searchInput.addEventListener('input', selectText);
+// searchInput.addEventListener('input', selectText);
 
-function selectText() {
-  const searchTerm = searchInput.value.trim();
-  if (searchTerm === '') {
-    removeAllSelections();
-    return;
-  }
+// function selectText() {
+//   const searchTerm = searchInput.value.trim();
+//   if (searchTerm === '') {
+//     removeAllSelections();
+//     return;
+//   }
 
-  removeAllSelections();
+//   removeAllSelections();
 
-  allTextElements.forEach(element => {
-    const text = element.textContent;
-    const regex = new RegExp(`(${escapeRegExp(searchTerm)})`, 'gi');
-    const matches = text.match(regex);
-    if (matches) {
-      matches.forEach(match => selectInElement(element, match));
-    }
-  });
-}
+//   allTextElements.forEach(element => {
+//     const text = element.textContent;
+//     const regex = new RegExp(`(${escapeRegExp(searchTerm)})`, 'gi');
+//     const matches = text.match(regex);
+//     if (matches) {
+//       matches.forEach(match => selectInElement(element, match));
+//     }
+//   });
+// }
 
-function removeAllSelections() {
-  if (window.getSelection) {
-    const selection = window.getSelection();
-    selection.removeAllRanges();
-  }
-}
+// function removeAllSelections() {
+//   if (window.getSelection) {
+//     const selection = window.getSelection();
+//     selection.removeAllRanges();
+//   }
+// }
 
-function selectInElement(element, searchTerm) {
-  const regex = new RegExp(`(${escapeRegExp(searchTerm)})`, 'gi');
-  const matches = element.textContent.match(regex);
-  if (matches) {
-    const range = document.createRange();
-    range.selectNodeContents(element);
-    matches.forEach(match => {
-      const start = element.textContent.indexOf(match);
-      range.setStart(element.firstChild, start);
-      range.setEnd(element.firstChild, start + match.length);
-      const selection = window.getSelection();
-      selection.addRange(range);
-    });
-  }
-}
+// function selectInElement(element, searchTerm) {
+//   const regex = new RegExp(`(${escapeRegExp(searchTerm)})`, 'gi');
+//   const matches = element.textContent.match(regex);
+//   if (matches) {
+//     const range = document.createRange();
+//     range.selectNodeContents(element);
+//     matches.forEach(match => {
+//       const start = element.textContent.indexOf(match);
+//       range.setStart(element.firstChild, start);
+//       range.setEnd(element.firstChild, start + match.length);
+//       const selection = window.getSelection();
+//       selection.addRange(range);
+//     });
+//   }
+// }
 
-function escapeRegExp(string) {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-}
+// function escapeRegExp(string) {
+//   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+// }
--- js/scroll.js
+++ js/scroll.js
@@ -8,5 +8,35 @@ window.addEventListener("scroll", function () {
   for (var i = 0; i < elementosTexto_1.length; i++) {
     elementosTexto_1[i].classList.toggle("scrollou", window.scrollY > 350);
   }
+
+  var elementosTexto_2 = document.querySelectorAll(".scrollTexto_2")
+  for (var i = 0; i < elementosTexto_2.length; i++) {
+    elementosTexto_2[i].classList.toggle("scrollou", window.scrollY > 650);
+  }
+
+  var elementosTexto_3 = document.querySelectorAll(".scrollTexto_3")
+  for (var i = 0; i < elementosTexto_3.length; i++) {
+    elementosTexto_3[i].classList.toggle("scrollou", window.scrollY > 1050);
+  } 
+
+  var elementosTexto_4 = document.querySelectorAll(".scrollTexto_4")
+  for (var i = 0; i < elementosTexto_4.length; i++) {
+    elementosTexto_4[i].classList.toggle("scrollou", window.scrollY > 1550);
+  } 
+
+  var elementosTexto_5 = document.querySelectorAll(".scrollTexto_5")
+  for (var i = 0; i < elementosTexto_5.length; i++) {
+    elementosTexto_5[i].classList.toggle("scrollou", window.scrollY > 1950);
+  } 
+
+  var elementosTexto_6 = document.querySelectorAll(".scrollTexto_6")
+  for (var i = 0; i < elementosTexto_6.length; i++) {
+    elementosTexto_6[i].classList.toggle("scrollou", window.scrollY > 2050);
+  }
+
+  var elementosTexto_7 = document.querySelectorAll(".scrollTexto_7")
+  for (var i = 0; i < elementosTexto_7.length; i++) {
+    elementosTexto_7[i].classList.toggle("scrollou", window.scrollY > 2100);
+  }
 })
 
