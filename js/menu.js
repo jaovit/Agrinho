4d8e464f365918cb90c7c9000e2dda6477027c2e
jão <110256135+jaovit@users.noreply.github.com>
2023-08-04 16:33:16 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,124 @@
+var aberto = false
+var checkbox = document.querySelector('#menu_input');
+
+document.addEventListener('click', function (event) {
+    var target = event.target;
+    var label = document.querySelector('.clic_js');
+    var nav = document.querySelector('.configuracao');
+    if (target === checkbox) {
+
+    } else {
+
+
+        if (target === label) {
+            if (aberto) {
+                aberto = false
+                checkbox.checked = true;
+
+            } else {
+                aberto = true;
+                checkbox.checked = false;
+            }
+        } else {
+
+            if (target !== checkbox && target !== nav) {
+                checkbox.checked = false;
+                if (aberto) {
+                    aberto = false
+                    checkbox.checked = false;
+
+                }
+            }
+        }
+    }
+
+});
+
+var nav = document.querySelector('.configuracao')
+nav.addEventListener('click', function (event) {
+    event.stopPropagation();
+});
+
+var input = document.querySelector('.menu_audio input');
+var menuAudio = document.querySelector('.menu_audio')
+
+menuAudio.classList.add("transition")
+input.addEventListener("input", menu_audio) 
+
+function menu_audio(){
+    if (input.checked) {
+        menuAudio.classList.add('menuAudio_aberto')
+    } else {
+        menuAudio.classList.remove('menuAudio_aberto')
+    }
+}
+
+
+// input type file 
+
+var Input = document.getElementById('input_foto');
+var Label = document.getElementById('label_foto');
+var texto_label = document.querySelector(".textolabel")
+
+Input.addEventListener('change', function() {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function() {
+      Label.style.backgroundImage = `url('${reader.result}')`;
+    };
+    reader.readAsDataURL(file);
+    texto_label.style.cssText = "display: none;"
+  } else {
+    Label.style.backgroundImage = 'none';
+  }
+});
+
+var fundo_element = document.querySelector(".formularios")
+var compartilhar_element = document.querySelector(".compartilhar")
+var contato_element = document.querySelector(".contato")
+var entrar_element = document.querySelector(".entrar")
+
+function compartilhar(){
+    checkbox.checked = false;
+    entrar_element.style.cssText = "display: none"
+    contato_element.style.cssText = "display: none"
+    compartilhar_element.style.cssText = "display: block"
+    fundo_element.style.cssText = "display: block"
+}
+
+function contato(){
+    checkbox.checked = false;
+    entrar_element.style.cssText = "display: none"
+    contato_element.style.cssText = "display: block"
+    compartilhar_element.style.cssText = "display: none"
+    fundo_element.style.cssText = "display: block"
+}
+
+function entrar(){
+    checkbox.checked = false;
+    entrar_element.style.cssText = "display: block"
+    contato_element.style.cssText = "display: none"
+    compartilhar_element.style.cssText = "display: none"
+    fundo_element.style.cssText = "display: block"
+}
+
+function fechar(){
+    checkbox.checked = false;
+    entrar_element.style.cssText = "display: none"
+    contato_element.style.cssText = "display: none"
+    compartilhar_element.style.cssText = "display: none"
+    fundo_element.style.cssText ="display: none"
+}
+
+function nativo(){
+    fechar()
+    compartilhar()
+	if (navigator.share !== undefined) {
+		navigator.share({
+			title: 'Sustentabilidade: Ações que mudam o mundo',
+			text: 'Um texto de resumo',
+			url: 'https://agrinho2023-iota.vercel.app/',
+		})
+	}
+}
